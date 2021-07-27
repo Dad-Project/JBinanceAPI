3b9f884ae554e3ceceb1369c0256165f11f22dc2
RowlaxX <32131492+RowlaxX@users.noreply.github.com>
2021-07-27 13:47:42 +0200

    Second Commit

--- src/main/java/fr/rowlaxx/binanceapi/core/market/ExchangeInformation.java
+++ src/main/java/fr/rowlaxx/binanceapi/core/market/ExchangeInformation.java
@@ -1,10 +1,15 @@
 package fr.rowlaxx.binanceapi.core.market;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import fr.rowlaxx.binanceapi.enums.Filters;
+import fr.rowlaxx.binanceapi.enums.Filters.Type;
 import fr.rowlaxx.binanceapi.utils.json.BinanceResponseJSON;
 import fr.rowlaxx.binanceapi.utils.json.JsonMap;
 import fr.rowlaxx.binanceapi.utils.json.JsonValue;
@@ -14,7 +19,6 @@ public abstract class ExchangeInformation<T extends Symbol> extends BinanceRespo
 
 	//Variables
 	@JsonValue private List<RateLimit> rateLimits;
-	@JsonValue private List<String> exchangeFilters;
 	@JsonValue private String timezone;
 	@JsonValue private long serverTime;
 	
@@ -22,14 +26,34 @@ public abstract class ExchangeInformation<T extends Symbol> extends BinanceRespo
 	@JsonMap(key = "symbol")
 	private Map<String, T> symbols;
 	
+	private Map<Filters, Filter> exchangeFilters;
+	
 	//Constructeurs
 	public ExchangeInformation(JSONObject response) {
 		super(response);
+		final HashMap<Filters, Filter> filters = new HashMap<>();
+		final JSONArray array = response.getJSONArray("exchangeFilters");
+		JSONObject json;
+		Filter exchangeFilter;
+		for (int i = 0 ; i < array.length() ; i++) {
+			json = array.getJSONObject(i);
+			exchangeFilter = Filter.fromJson(json);
+			filters.put(exchangeFilter.getFilterType(), exchangeFilter);
+		}
+		this.exchangeFilters = Collections.unmodifiableMap(filters);
 	}
 	
 	//Getters
-	public List<String> getExchangeFilters() {
+	public Map<Filters, Filter> getExchangeFilters() {
 		return exchangeFilters;
+	}
+	
+	public Filter getExchangeFilter(Filters filter) {
+		if (filter == null)
+			return null;
+		if (filter.getType() == Type.EXCHANGE)
+			return exchangeFilters.get(filter);
+		throw new IllegalArgumentException("filter may be a EXCHANGE filter.");
 	}
 	
 	public List<RateLimit> getRateLimits() {
@@ -42,6 +66,10 @@ public abstract class ExchangeInformation<T extends Symbol> extends BinanceRespo
 	
 	public Map<String, T> getSymbols() {
 		return symbols;
+	}
+	
+	public T getSymbol(String symbol) {
+		return symbols.get(symbol);
 	}
 	
 	public String getTimezone() {
--- src/main/java/fr/rowlaxx/binanceapi/core/market/Symbol.java
+++ src/main/java/fr/rowlaxx/binanceapi/core/market/Symbol.java
@@ -1,27 +1,45 @@
 package fr.rowlaxx.binanceapi.core.market;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import fr.rowlaxx.binanceapi.enums.Filters;
+import fr.rowlaxx.binanceapi.enums.Filters.Type;
 import fr.rowlaxx.binanceapi.enums.OrderTypes;
 import fr.rowlaxx.binanceapi.utils.json.BinanceResponseJSON;
+import fr.rowlaxx.binanceapi.utils.json.JsonValue;
 
 public abstract class Symbol extends BinanceResponseJSON {
 	private static final long serialVersionUID = -6654992662838138776L;
 
 	//Variables
-	private String symbol;
-	private String baseAsset;
-	private String quoteAsset;
-	private int baseAssetPrecision;
-	private Filter filters;
-	private int quotePrecision;
-	private Set<OrderTypes> orderTypes;
+	@JsonValue private String symbol;
+	@JsonValue private String baseAsset;
+	@JsonValue private String quoteAsset;
+	@JsonValue private int baseAssetPrecision;
+	@JsonValue private int quotePrecision;
+	@JsonValue private Set<OrderTypes> orderTypes;
+	
+	private Map<Filters, Filter> filters;
 	
 	//Constructeurs
 	public Symbol(JSONObject response) {
 		super(response);
+		final HashMap<Filters, Filter> filters = new HashMap<>();
+		final JSONArray array = response.getJSONArray("filters");
+		Filter filter;
+		JSONObject json;
+		for (int i = 0 ; i < array.length() ; i++) {
+			json = array.getJSONObject(i);
+			filter = Filter.fromJson(json);
+			filters.put(filter.getFilterType(), filter);
+		}
+		this.filters = Collections.unmodifiableMap(filters);
 	}
 	
 	//Getters
@@ -33,8 +51,16 @@ public abstract class Symbol extends BinanceResponseJSON {
 		return baseAssetPrecision;
 	}
 	
-	public Filter getFilters() {
+	public Map<Filters, Filter> getFilters() {
 		return filters;
+	}
+	
+	public Filter getFilter(Filters filter) {
+		if (filter == null)
+			return null;
+		if (filter.getType() == Type.SYMBOL)
+			return filters.get(filter);
+		throw new IllegalArgumentException("filter may be a SYMBOL filter.");
 	}
 	
 	public Set<OrderTypes> getOrderTypes() {
