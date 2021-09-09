0de625885254f9d135e9d08fc0d23e1c87a56ee6
Theo <Theo@DESKTOP-KAFEGGD>
2021-09-09 18:01:01 +0200

    Add some method for Candlestick interface

--- src/main/java/fr/rowlaxx/binanceapi/core/market/Candlestick.java
+++ src/main/java/fr/rowlaxx/binanceapi/core/market/Candlestick.java
@@ -13,4 +13,27 @@ public interface Candlestick {
 		return getClose() - getOpen();
 	}
 	
+	default double getChangeUp() {
+		return Math.max(0, getChange());
+	}
+	
+	default double getChangeDown() {
+		return -Math.min(0, getChange());
+	}
+	
+	default double getAbsChange() {
+		return Math.abs(getChange());
+	}
+	
+	default double getHL2() {
+		return (getHigh() + getLow()) / 2;
+	}
+	
+	default double getHLC3() {
+		return (getHigh() + getLow() + getClose()) / 3;
+	}
+	
+	default double getOHLC4() {
+		return (getHigh() + getLow() + getClose() + getOpen()) / 4;
+	}
 }
