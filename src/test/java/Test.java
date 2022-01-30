b08207c8ca7b545adfbf1e51b05cf95e22bdcf76
RowlaxX <32131492+RowlaxX@users.noreply.github.com>
2022-01-30 19:57:28 +0100

    Added a Test class

---
+++ src/test/java/Test.java
@@ -0,0 +1,13 @@
+import fr.rowlaxx.binanceapi.client.BinanceClient;
+
+public class Test {
+
+	private static BinanceClient client;
+	
+	public static void main(String[] args) {
+		
+		client.spot().trade().po
+		
+	}
+
+}
