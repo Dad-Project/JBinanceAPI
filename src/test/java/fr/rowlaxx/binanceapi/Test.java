1a3d14802ad962112643748e75b7281dc6f88005
RowlaxX <32131492+RowlaxX@users.noreply.github.com>
2022-01-31 20:03:34 +0100

    Test class

--- src/test/java/fr/rowlaxx/binanceapi/Test.java
+++ src/test/java/fr/rowlaxx/binanceapi/Test.java
@@ -1,5 +1,9 @@
 package fr.rowlaxx.binanceapi;
 import fr.rowlaxx.binanceapi.client.BinanceClient;
+import fr.rowlaxx.binanceapi.core.margin.MarginInterestRecord;
+import fr.rowlaxx.binanceapi.core.margin.PriceIndex;
+import fr.rowlaxx.binanceapi.core.options.trade.FundingFlow;
+import fr.rowlaxx.binanceapi.core.wallet.FundingWallet;
 
 public class Test {
 
