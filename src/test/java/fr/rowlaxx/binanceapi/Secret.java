2d0ab443f53e1f71edf9c826a2823a6b3644c349
theo <theo@RowlaxX-L>
2022-03-19 01:03:03 +0100

    Update

--- src/test/java/fr/rowlaxx/binanceapi/Secret.java
+++
@@ -1,8 +0,0 @@
-package fr.rowlaxx.binanceapi;
-
-public class Secret {
-
-	static final String API_KEY = "dovipQ69GQoDQM5dwMgqa1kagajw04wjOrykKUdpcT8on3S6vTsArHiGZSVz0dMA";
-	static final String API_SECRET = "FslSQFkoeBehb89w0mb5LNBtOyCLI20nB35I9L5YFwyomgHxzEZxPfEhL5NUcnRl";
-	
-}
