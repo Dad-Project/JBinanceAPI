b81322d9c8885c8f9b35f14dce89d31e7074188f
theo <theo@RowlaxX-L>
2022-04-04 10:54:12 +0200

    Test

--- src/test/java/fr/rowlaxx/binanceapi/Test.java
+++ src/test/java/fr/rowlaxx/binanceapi/Test.java
@@ -1,25 +1,39 @@
 package fr.rowlaxx.binanceapi;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.concurrent.ExecutionException;
 
-import fr.rowlaxx.binanceapi.api.ApiImplementer;
+import fr.rowlaxx.binanceapi.api.spot.SpotMarketDataStreamAPI.OnAggTrade;
+import fr.rowlaxx.binanceapi.api.spot.SpotMarketDataStreamAPI.OnTrade;
 import fr.rowlaxx.binanceapi.client.BinanceClient;
-import fr.rowlaxx.binanceapi.core.savings.FixedProduct;
-import fr.rowlaxx.binanceapi.core.savings.FixedTypes;
-import fr.rowlaxx.binanceapi.core.savings.FlexibleProduct;
-import fr.rowlaxx.binanceapi.core.savings.SavingsStatus;
+import fr.rowlaxx.binanceapi.core.CompressedTrade;
+import fr.rowlaxx.binanceapi.core.spot.stream.SpotStreamTrade;
 
 public class Test {	
 
 	private static BinanceClient client;
-	
-	public static void main(String[] args) throws IOException {
+
+	public static void main(String[] args) throws IOException, InterruptedException, ExecutionException, URISyntaxException {
 		client = BinanceClient.create(args[0], args[1]);
 		client.login();
-		ApiImplementer.DEBUG = true;
 		
-		for (FixedProduct a : client.savings().getFixedProducts(FixedTypes.ACTIVITY, 1, 100)) {
-			System.out.println(a);
-		}
+		client.spot().stream().addOnAggTradeEvent(new OnAggTrade() {
+			
+			@Override
+			public void onAggTrade(String symbol, CompressedTrade trade) {
+				System.out.println(trade);
+			}
+		});
+		
+		client.spot().stream().subscribeAggTrade("btcusdt");
+		
+		
+		Thread.sleep(20000);
+		client.spot().stream().close();
+		
+	
 	}
+
+
 }
