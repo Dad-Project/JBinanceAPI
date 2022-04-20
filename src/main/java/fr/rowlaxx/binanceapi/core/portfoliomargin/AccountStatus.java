0af854cb443623eef9923a75c9af25ad17823729
Theo <Theo@DESKTOP-SLV4CDM>
2022-04-20 18:32:09 +0200

    Added Enum for Account Status

---
+++ src/main/java/fr/rowlaxx/binanceapi/core/portfoliomargin/AccountStatus.java
@@ -0,0 +1,7 @@
+package fr.rowlaxx.binanceapi.core.portfoliomargin;
+
+public enum AccountStatus {
+
+	NORMAL, MARGIN_CALL, SUPPLY_MARGIN, REDUCE_ONLY, ACTIVE_LIQUIDATION, FORCE_LIQUIDATION, BANKRUPTED;
+	
+}
