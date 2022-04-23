8d81a6d66535e11edec9a88e20bbee2903ec3e79
theo <theo@RowlaxX-L>
2022-04-23 11:15:13 +0200

    Added user stream to SpotAPI

--- src/main/java/fr/rowlaxx/binanceapi/api/spot/SpotAPI.java
+++ src/main/java/fr/rowlaxx/binanceapi/api/spot/SpotAPI.java
@@ -1,27 +1,47 @@
 package fr.rowlaxx.binanceapi.api.spot;
 
-public final class SpotAPI {
+import java.util.Objects;
 
-	private final SpotMarketDataAPI market;
-	private final SpotTradeAPI trade;
-	private SpotMarketDataStreamAPI stream;
+import fr.rowlaxx.binanceapi.api.ApiImplementer;
+import fr.rowlaxx.binanceapi.client.BinanceClient;
+
+public final class SpotAPI {
 	
-	public SpotAPI(SpotMarketDataAPI market, SpotTradeAPI trade) {
-		this.trade = trade;
-		this.market = market;
+	//Variables
+	private final BinanceClient client;
+	
+	private SpotMarketDataAPI market = null;
+	private SpotTradeAPI trade = null;	
+	private SpotMarketStreamAPI marketstream = null;
+	private SpotUserStreamAPI userstream = null;
+	
+	//Constructeurs
+	public SpotAPI(BinanceClient client) {
+		this.client = Objects.requireNonNull(client, "client may not be null.");
 	}
 	
+	//Methodes
 	public SpotMarketDataAPI market() {
+		if (market == null)
+			market = ApiImplementer.implementz(SpotMarketDataAPI.class, client);
 		return market;
 	}
 	
 	public SpotTradeAPI trade() {
+		if (trade == null)
+			trade = ApiImplementer.implementz(SpotTradeAPI.class, client);
 		return trade;
 	}
 	
-	public SpotMarketDataStreamAPI stream() {
-		if (stream == null)
-			stream = new SpotMarketDataStreamAPI();
-		return stream;
+	public SpotMarketStreamAPI marketstream() {
+		if (marketstream == null)
+			marketstream = new SpotMarketStreamAPI();
+		return marketstream;
+	}
+	
+	public SpotUserStreamAPI userstream() {
+		if (userstream == null)
+			userstream = new SpotUserStreamAPI(client.getHttpClient(), "/api/v3/userDataStream");
+		return userstream;
 	}
 }
