a4e5cc7fab8aea30bdc1d18a405d1019cab51b52
theo <theo@RowlaxX-L>
2022-06-06 23:33:00 +0200

    Test

--- src/test/java/fr/rowlaxx/binanceapi/Test.java
+++ src/test/java/fr/rowlaxx/binanceapi/Test.java
@@ -2,13 +2,11 @@ package fr.rowlaxx.binanceapi;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.concurrent.ExecutionException;
 
-import fr.rowlaxx.binanceapi.api.spot.SpotUserStreamAPI.OnAccountUpdate;
+import fr.rowlaxx.binanceapi.api.ApiImplementer;
 import fr.rowlaxx.binanceapi.client.BinanceClient;
-import fr.rowlaxx.binanceapi.core.Asset;
+import fr.rowlaxx.binanceapi.core.usdm.marketdata.UsdmExchangeInformation;
 
 public class Test {	
 
@@ -17,14 +15,9 @@ public class Test {
 	public static void main(String[] args) throws IOException, InterruptedException, ExecutionException, URISyntaxException {
 		client = BinanceClient.create(args[0], args[1]);
 		
-		client.spot().userstream().addOnAccountUpdateEvent(new OnAccountUpdate() {
-			
-			@Override
-			public void onAccountUpdate(long lastUpdate, Map<String, Asset> newBalance) {
-				for (Entry<String, Asset> entry : newBalance.entrySet())
-					System.out.println(entry.getValue());
-			}
-		});
+		ApiImplementer.DEBUG = true;
+		
+		client.coinm().market().getExchangeInformation();
 	}
 
 
